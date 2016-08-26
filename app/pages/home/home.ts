fc727af66b74631430b7f50071be5f843b05a917
aldruin <batur.activeden@gmail.com>
2016-08-27 02:47:17 +0300

    #5 Added message form

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -8,8 +8,19 @@ import { WebSockets } from '../../providers/web-socket/web-socket';
 })
 export class HomePage {
   public message = "aldruin";
+  public chatMessages: Array<Object> = new Array();
 
-  constructor(private ws: WebSockets, private nav: NavController) {}
+  constructor(private ws: WebSockets, private nav: NavController) {
+    this.listenMessage();
+  }
+
+  listenMessage() {
+    this.ws.listen(e => {
+      this.chatMessages.push(e.data);
+      
+      console.log(e.data);
+    })
+  }
 
   sendMessage() {
     this.ws.send(this.message);
