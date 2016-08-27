fc6adea9aa9b57c7f1378c9d8b096e072499f55d
aldruin <batur.activeden@gmail.com>
2016-08-27 13:54:56 +0300

    #7 Reworked chat structure

---
+++ app/components/chat-scroll/chat-scroll.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, AfterViewInit, AfterViewChecked } from '@angular/core';
+
+@Directive({
+  selector: '[chat-scroll]'
+})
+export class ChatScroll implements AfterViewInit, AfterViewChecked {
+	private scrollHandler
+
+  constructor(private element: ElementRef) {}
+
+	ngAfterViewInit(){
+    this.scrollHandler = this.element.nativeElement.getElementsByTagName("scroll-content")[0];
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();        
+  }
+
+  scrollToBottom(){
+		this.scrollHandler.scrollTop = this.scrollHandler.scrollHeight;
+  }
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WebSockets } from '../../providers/web-socket/web-socket';
+import { ChatScroll } from '../../components/chat-scroll/chat-scroll'
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
-  providers: [WebSockets]
+  providers: [WebSockets],
+  directives: [ChatScroll]
 })
 export class HomePage {
-  public message = "aldruin";
-  public chatMessages: Array<Object> = new Array();
+  public message = "aldruin"; // Message field model
+  public chatMessages: Array<Object> = new Array(); // Main messages array
 
   constructor(private ws: WebSockets, private nav: NavController) {
-    this.listenMessage();
-  }
-
-  listenMessage() {
+    // Setting receive message callback
     this.ws.listen(e => {
       this.chatMessages.push(e.data);
-      
-      console.log(e.data);
     })
   }
 
